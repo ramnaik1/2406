140284249ae0080ecd00a601a386db583188fc11
ramnaik1 <107945935+ramnaik1@users.noreply.github.com>
2022-07-03 16:04:31 +0530

    Update naik.sh

--- naik.sh
+++ naik.sh
@@ -1,4 +1,4 @@
-pipeline
+pipeline{
 agent any
 stage
 {
@@ -7,19 +7,23 @@ stage ('Build')
 echo "Building the code ............"
 bat "mvn clean"
 }
+}
 stage ('Test')
 {
 echo "Test the code ............"
 bat "mvn test"
+}
 }
 stage ('Compile')
 {
 echo "compiling the project ............"
 bat "mvn compile"
 }
+}
 stage ('deploy')
 {
 steps{
 echo "deploying the project ............"
+}
 }
 }
